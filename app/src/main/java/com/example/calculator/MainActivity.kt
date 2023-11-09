6fa6f14005a7b7b433672f6cb5ce4ff90ccc26bc
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-11-09 20:06:32 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), View.OnClickListener
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnLomie: Button = findViewById(R.id.btn_lomie)
+        btnLomie.setOnClickListener(this)
+    }
+    override fun onClick(v: View?)
+    {
+        when (v?.id)
+        {
+            R.id.btn_lomie ->
+            {
+                val moveIntent = Intent(this@MainActivity, LomieActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
